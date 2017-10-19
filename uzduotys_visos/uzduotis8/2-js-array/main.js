e5acec88a610e77f5ad7ffe4d2d65b910170074d
tautvydascoding <tautvydascoding@gmail.com>
2017-10-19 11:18:27 +0300

    aa

--- uzduotys_visos/uzduotis8/2-js-array/main.js
+++ uzduotys_visos/uzduotis8/2-js-array/main.js
@@ -35,5 +35,59 @@ delete bag[2]; // lieka skyle "undefiend"
 console.log("istrinname prozektoriu:", bag);
 
 
+// copy 3 elementa
+var tuscias = bag.slice(2, 5);    //  2- nuo antro   3- iki kelinto (indekso nr)
+console.log("Senas sarasas", bag);
+console.log("Naujas sarasas", tuscias);
+
+bag = bag.concat(tuscias);  // sujungiu 2 masyvus
+console.log("po sujungimo:", bag);
+
+// uzduotis
+// 2. i masyvo pradzia ideti elmenta “ziebtuvelis”
+bag.unshift("ziebtuvelis");
+console.log("add ziebtuvelis:", bag);
+// 3. istrinti 3 elmenta
+delete bag[2];
+bag.splice(2, 1);
+console.log("istriname 3 ir 4", bag);
+
+
+
+
+
+// 4. sukurti masyva (50 ilgumo) naudojant FOR ir uzpildyti skaiciu 0 (kiekvienas stalcius turi but lygus "0");
+var sarasas = [];
+for (var i = 0; i < 50; i++) {
+    sarasas[i] = 0;
+}
+console.log("sarasas nuliu: ", sarasas);
+
+
+//
+// 5.2: kas antra ^ masyvo elementa pakeisti i "3"
+// [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, ....
+
+
+// for (var i = 1; i < sarasas.length; i+=2) {
+//     sarasas[i] = 3;
+// }
+// console.log("sarasas su 3: ", sarasas);
+
+// arba
+for (var i = 1; i < 26; i++) {
+    sarasas[ (i*2)-2 ] = 1;
+    sarasas[ (i*2)-1 ] = 3;
+}
+console.log("sarasas su 3: ", sarasas);
+
+//  5.2: kas penkta ^ masyvo elementa pakeisti i "99"
+
+
+
+
+
+
+
 
 //
---
+++ uzduotys_visos/uzduotis9/tautvydas/1-if/main.js
@@ -0,0 +1,51 @@
+console.log("labas vakaras");
+
+
+var skaicius1 = 123;
+var skaicius2 = 44;
+
+// pasakyti, kuris skiscius didziausias
+if ( skaicius1 > skaicius2 ) {
+    console.log('skaicius1 it\'\'s  yra didziausias \n  \t  test');
+} else {
+    console.log("skaicius2 it\'s  yra didziausias");
+}
+
+var skaicius3 = 9999;
+
+// rasti didziausia skaiciu
+if ( skaicius1 > skaicius2) {
+    if ( skaicius1 > skaicius3 ) {
+        console.log("skaicius1 ");
+    } else {
+        console.log("skaicius3  arba lygus skaicius1");
+    }
+
+} else {
+    if ( skaicius2 > skaicius3) {
+        console.log("skaicius2");
+    } else {
+        console.log("skaicius3  arba lygus skaicius2");
+    }
+}
+
+
+if (skaicius1 > skaicius2 &&  skaicius1 > skaicius3) {
+    console.log("skaicius1 pats didziuasias");
+}
+if ( nesiprasias ||  nesusisukavias) {
+    console.log("neiles i restorana");
+}
+var age = 12;
+if ( age < 14 || age > 65) {
+    console.log("Simsonus");
+    if( age < 14) {
+        console.log("Pliusines varles");
+    } else { // pensininkai
+        console.log("Sen. sokiai");
+    }
+} else if ( age > 14 && age < 18 ) {
+    console.log("Nokia 399");
+} else if (true) {
+
+}
