95695d9f36258517c2262ec817e241e47d499468
tautvydascoding <tautvydascoding@gmail.com>
2017-10-23 09:38:12 +0300

    11 diena

--- uzduotys_visos/uzduotis10/5-jquery-basics/main.js
+++ uzduotys_visos/uzduotis10/5-jquery-basics/main.js
@@ -2,7 +2,13 @@ console.log("labas vakaras");
 
 document.querySelector("h1").innerHTML = "Pakeistas tekstas";
 
-$("h1")[0].innerHTML = "<h2>dar kart pakeistas</h2>";
+$("h1")[0].innerHTML = "<h1>dar kart pakeistas</h1>";
 
 // $(".container-fluid")
 // $("#karusele")
+
+$('h2').click( function(){
+
+    $('h3').toggle("slow");
+
+} );
