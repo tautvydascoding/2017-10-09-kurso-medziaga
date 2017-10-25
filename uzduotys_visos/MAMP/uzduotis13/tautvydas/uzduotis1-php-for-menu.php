1e03081f3f659e5acddb4ad8370c21d14a410557
tautvydascoding <tautvydascoding@gmail.com>
2017-10-25 16:16:09 +0300

    13

---
+++ uzduotys_visos/MAMP/uzduotis13/tautvydas/uzduotis1-php-for-menu.php
@@ -0,0 +1,9 @@
+// uzduotis 1
+
+// A. susikurti meniu masyva
+// B. atspausdinti visus meniu punktus i HTML
+
+// uzduotis 2
+// A . susirasti 6 nuotraukas
+// B susikurti nuotrauku pavadinimu masyva
+// C. atspausdinti visas nuotraukas su ciklu, po 3-ris i eilute
