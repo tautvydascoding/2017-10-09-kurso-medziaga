3202b6a4394cac60b738e270a68ced7384de335d
Tautvydas <arttgd@gmail.com>
2017-10-26 18:56:38 +0300

    15

--- php-hints.php
+++ php-hints.php
@@ -1,21 +1,34 @@
 
 //==========string======
 strlen()  - grazina teksto ilgi
+strcmp($x, $y)  // return 1 or 0 -1
+"." sujungimo zenklas   pvz.:   $z = $text1 . " " . $text2;
+$z .= $text1;
+$z .= $text2;
+
 
 //============arrays=========
 $human = array("name" => "Tomas", "lname" => "Tomulaitokas", "age" => 13);
+echo $human['age']; // 13;
 
 $text = "preke1 : preke2 : preke3 : preke4";
 $myArray = explode( " :", text);
+
+$array.count()
+$array.sizeof()
+
+
 //=================
 gettype() 
-
+is_array()
 
 // ========out put============
 print($name);
 echo "vardas $name";
 print_r($names);  // atspausdins masyva
 die("paskurtinatas tekstas ka matys vartotojas");
+printf("uz %s juru stovi %.2f ezerai", "juru", 2.5555);
+
 
 //==========session=====
 session_start(); 
