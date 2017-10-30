6998f22621ae152d84205d19872f7d030bf47258
tautvydascoding <tautvydascoding@gmail.com>
2017-10-30 10:28:23 +0200

    16

--- uzduotys_visos/MAMP/uzduotis16/tautvydas/16-img lent.php
+++ uzduotys_visos/MAMP/uzduotis16/tautvydas/16-img lent.php
@@ -17,7 +17,13 @@ INSERT INTO images  VALUES('', '33.gif', 1);
 
 SELECT * FROM images;
 
+INSERT INTO doctors VALUES (5, "Pietkus", "Leo");
+INSERT INTO doctors VALUES (6, "Markas", "Mantis");
+SELECT * FROM doctors;
 
+INSERT INTO patients VALUES (7, "Rokas", "Juodas", 1);
+INSERT INTO patients VALUES (8, "Tim", "Raudonas", 5);
+INSERT INTO patients VALUES (9, "Tom", "Baltas", 6);
 
 
 
