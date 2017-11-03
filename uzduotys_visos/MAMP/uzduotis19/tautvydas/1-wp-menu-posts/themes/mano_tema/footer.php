6a0bcef28c0caa6c698e9c50f917bd68e0f2d54b
tautvydascoding <tautvydascoding@gmail.com>
2017-11-03 10:22:01 +0200

    18

---
+++ uzduotys_visos/MAMP/uzduotis19/tautvydas/1-wp-menu-posts/themes/mano_tema/footer.php
@@ -0,0 +1,16 @@
+
+
+    <footer>
+        <?php
+            wp_nav_menu(array('theme_location' => 'footermenu'));
+          ?>
+        2017
+    </footer>
+
+
+    <?php
+        wp_footer();
+     ?>
+
+    </body>
+</html>
