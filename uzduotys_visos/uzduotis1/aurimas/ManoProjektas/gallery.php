a25fd3c322cb765f55f2acc54b6cd6d51f2d20e8
amigolituano <valdas@ezerelis.lt>
2017-11-03 14:09:51 +0200

    failai

--- uzduotys_visos/uzduotis1/aurimas/ManoProjektas/gallery.php
+++ uzduotys_visos/uzduotis1/aurimas/ManoProjektas/gallery.php
@@ -8,6 +8,7 @@
         <title>Galerija</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
         <link rel="stylesheet" href="./css/style.css">
+
     </head>
     <body id="gallery-body">
         <div class="container-fluid">
@@ -264,5 +265,13 @@
                 </div>
             </div>
         </div>
+
+        
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+        </script>
+        <script type="text/javascript" src="./js/gallery.js">
+
+        </script>
+
     </body>
 </html>
