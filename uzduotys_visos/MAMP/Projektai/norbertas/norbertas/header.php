d13fa48d6f4eca80645930f6509845f3a24af7d8
Norbertas5 <nnorbis5@gmail.com>
2017-11-06 14:32:50 +0200

    projektas

---
+++ uzduotys_visos/MAMP/Projektai/norbertas/norbertas/header.php
@@ -0,0 +1,27 @@
+
+        <div id="header" class="container-fluid">
+            <div class="row">
+                <div class="col-md-2 brandd">
+
+                </div>
+                <!-- nav start -->
+                <div class="col-md">
+                    <ul class="nav justify-content-around">
+                        <li class="nav-item headnav">
+                            <a class="nav-link active hnavtxt" href="#">main</a>
+                        </li>
+                        <li class="nav-item headnav">
+                            <a class="nav-link hnavtxt" href="./gallery.php">gallery</a>
+                        </li>
+                        <li class="nav-item headnav">
+                            <a class="nav-link hnavtxt " href="#scroll">contacts</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- nav end -->
+                <div class="col-md-2 loginali">
+                <a href="#" class="logintxt">login</a>
+
+                </div>
+            </div>
+        </div>
---
+++ uzduotys_visos/MAMP/Projektai/norbertas/norbertas/success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Success</title>
+    </head>
+    <body>
+        <h2>jusu pranesimas semingas issiustas</h2>
+        <a href="./index.php"><button>Gryzti</button></a>
+    </body>
+</html>
